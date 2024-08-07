0145eca626208ee12511a5b8b553cbc9e94bc000
Mr Footwork <pandau.ting@gmail.com>
2024-08-07 09:30:41 +0200

    fetch more than 10 categories

--- server/api/categories.ts
+++ server/api/categories.ts
@@ -2,6 +2,6 @@ import type { WP_REST_API_Categories } from 'wp-types';
 
 export default defineEventHandler(async () => {
     const baseUrl = useRuntimeConfig().public.wpRestApiBaseUrl
-    const categories = await $fetch(`${baseUrl}/categories`)
+    const categories = await $fetch(`${baseUrl}/categories?per_page=100`)
     return categories as WP_REST_API_Categories
 })
