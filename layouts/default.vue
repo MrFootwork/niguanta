165c4b3db5b9194852d89c8a058edaa4edab0e1a
Mr Footwork <pandau.ting@gmail.com>
2024-08-13 00:31:54 +0200

    add navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-    <header class="test">
+    <header class="ingrid-darling">
       Niguanta
     </header>
+    <nav>
+      <ul>
+        <li
+          v-for="category in categories"
+          :key="category.id"
+        >
+          <span>{{ category.name }} {{ category.id }}</span>
+        </li>
+      </ul>
+    </nav>
     <slot />
     <div>
       <div>Posts Count {{ postCount }}</div>
@@ -15,13 +25,26 @@
 
 <script setup lang="ts">
 import { usePostStore } from '@/stores/posts'
+import { useCategoryStore } from '@/stores/categories'
+
+const navbarItems = [
+  665,
+  843,
+]
 
 const postsStore = usePostStore()
 const postCount = computed(() => postsStore.postCount)
+
+const categoryStore = useCategoryStore()
+const { categories } = storeToRefs(categoryStore)
+
+onBeforeMount(() => {
+  if (categories.value.length === 0) {
+    categoryStore.fetchCategories()
+  }
+})
 </script>
 
 <style scoped lang="scss">
-.test {
-  color: red
-}
+
 </style>
