6aed0293a5d43ac47840f9415821c0d3adad6e72
Mr Footwork <pandau.ting@gmail.com>
2024-08-30 14:40:19 +0200

    use useMail() again

--- pages/contact.vue
+++ pages/contact.vue
@@ -54,22 +54,27 @@ const form = ref({
 const successMessage = ref('')
 const errorMessage = ref('')
 
-// const mail = useMail()
+const mail = useMail()
 
 async function submitForm() {
   try {
-    // mail.send({
-    //   from: form.value.name,
-    //   subject: 'A reader wants to contact you!',
-    //   recipient: form.value.email,
-    //   text: `
-    //     Name: ${form.value.name}
-    //     \nEmail: ${form.value.email}
-    //     \nMessage: ${form.value.message}`,
-    //   // FIXME Try email templates via https://nuxt.com/modules/vue-email
-    //   html: `<h1>My HTML</h1>
-    //   <p>${form.value.message}</p>`,
-    // })
+    mail.send({
+      from: form.value.name,
+      subject: 'A reader wants to contact you!',
+      recipient: form.value.email,
+      text: `
+        Name: ${form.value.name}
+        \nEmail: ${form.value.email}
+        \nMessage: ${form.value.message}`,
+      // FIXME Try email templates via https://nuxt.com/modules/vue-email
+      html: `
+        <h1>My HTML</h1>
+        <ul>
+          <li>Name: ${form.value.name}</li>
+          <li>Mail: ${form.value.email}</li>
+          <li>Message: <p>${form.value.message}</p></li>
+        </ul>`,
+    })
 
     successMessage.value = 'Your message has been sent successfully!'
     errorMessage.value = ''
